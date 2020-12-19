189efc26ac943d7d362e5d33b2c1c5c4e4e30e14
yanuar ridwan <yanuarridwan@yanuars-MacBook-Pro.local>
2020-12-19 23:23:06 +0700

    style halaman daftar

--- application/views/admin/daftar.php
+++ application/views/admin/daftar.php
@@ -1,36 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<!-- basic -->
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<!-- mobile metas -->
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="viewport" content="initial-scale=1, maximum-scale=1">
-<!-- site metas -->
-<title>PPDB</title>
-<meta name="keywords" content="">
-<meta name="description" content="">
-<meta name="author" content="">
-<!-- site icons -->
-<link rel="icon" href="images/fevicon/fevicon.png" type="image/gif" />
-<!-- bootstrap css -->
-<link rel="stylesheet" href="<?php echo base_url('css/bootstrap.min.css') ?>"/>
-<!-- Site css -->
-<link rel="stylesheet" href="<?php echo base_url('css/style.css') ?>" />
-<!-- responsive css -->
-<link rel="stylesheet" href="<?php echo base_url('css/responsive.css') ?>" />
-<!-- colors css -->
-<link rel="stylesheet" href="<?php echo base_url('css/colors1.css') ?>" />
-<!-- custom css -->
-<link rel="stylesheet" href="<?php echo base_url('css/custom.css') ?>" />
-<!-- wow Animation css -->
-<link rel="stylesheet" href="<?php echo base_url('css/animate.css') ?>" />
-<!-- revolution slider css -->
-<link rel="stylesheet" type="text/css" href="<?php echo base_url('revolution/css/settings.css') ?>" />
-<link rel="stylesheet" type="text/css" href="<?php echo base_url('revolution/css/layers.css') ?>" />
-<link rel="stylesheet" type="text/css" href="<?php echo base_url('revolution/css/navigation.css') ?>" />
-<!--[if lt IE 9]>
+  <!-- basic -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!-- mobile metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+  <!-- site metas -->
+  <title>PPDB</title>
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <!-- site icons -->
+  <link rel="icon" href="images/fevicon/fevicon.png" type="image/gif" />
+  <!-- bootstrap css -->
+  <link rel="stylesheet" href="<?php echo base_url('css/bootstrap.min.css') ?>" />
+  <!-- Site css -->
+  <link rel="stylesheet" href="<?php echo base_url('css/style.css') ?>" />
+  <!-- responsive css -->
+  <link rel="stylesheet" href="<?php echo base_url('css/responsive.css') ?>" />
+  <!-- colors css -->
+  <link rel="stylesheet" href="<?php echo base_url('css/colors1.css') ?>" />
+  <!-- custom css -->
+  <link rel="stylesheet" href="<?php echo base_url('css/custom.css') ?>" />
+  <!-- wow Animation css -->
+  <link rel="stylesheet" href="<?php echo base_url('css/animate.css') ?>" />
+  <!-- revolution slider css -->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url('revolution/css/settings.css') ?>" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url('revolution/css/layers.css') ?>" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url('revolution/css/navigation.css') ?>" />
+  <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
       <![endif]-->
@@ -41,85 +42,70 @@
 <!-- End Header -->
 
 <!-- end inner page banner -->
-<div class="section padding_layout_1">
+<div class="section">
   <div class="container">
     <div class="row">
-      <div class="col-xl-3 col-lg-4 col-md-8 col-sm-8 col-xs-12"></div>
-      <div class="col-xl-6 col-lg-8 col-md-8 col-sm-8 col-xs-12">
-        <div class="row">
-          <div class="full">
-            <div class="col">
-              <div class="main_heading text_align_center">
-                <h2>Daftar	Peserta Didik Baru</h2>
-              </div>
-            </div>
-            <div class="row">
-            <div class="col-lg-12">
-              <div class="checkout-form">
-                <form action="#">
-                  <fieldset>
-                  <div class="row">
-                    <div class="col-md-6">
-                      <div class="form-field" required>
-                        <label>Nama Lengkap <span class="red">*</span></label>
-                        <div class="col-xs-12">
-                        <input name="fn" type="text">
+      <div class="row">
+        <div class="full">
+          <div class="d-flex h-100 justify-content-center align-items-center">
+            <div class="card-body">
+              <div class="payment-form">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                  <div class="main_heading text_align_center">
+                    <h2>Daftar Peserta Didik Baru</h2>
+                  </div>
+                </div>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 appointment_form">
+                  <div class="form_section">
+                    <form class="form_contant" action="index.html" style="padding-left: 200px; padding-right:200px; width:100%; display:block;">
+                      <div class="row">
+                        <div class="field col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                          <label for="ttl">Nama Lengkap</label>
+                          <input class="field_custom" placeholder="Nama Lengkap" type="text" required>
+                        </div>
+                        <div class="field col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                          <label for="ttl">TTL</label>
+                          <input class="field_custom" placeholder="Tempat Lahir" type="text" id="ttl" required>
+                        </div>
+                        <div class="field col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                          <input class="field_custom" placeholder="Tanggal Lahir" type="date" required style="margin-top: 25px;">
                         </div>
                       </div>
-                    </div>
-                    <div class="col-md-6">
-                      <div class="form-field">
-                        <label>TTL <span class="red">*</span></label>
-                        <div class="col-xs-12">
-                        <input name="ln" type="text">
-                      </div>
-                     </div>
-                    </div>
-                    <div class="col-md-6">
-                      <div class="form-field">
-                        <label>Email <span class="red">*</span></label>
-                        <div class="col-xs">
-                        <input name="cm" type="text">
-                      </div>
-                     </div>
-                    </div>
-                    <div class="col-md-6">
-                      <div class="form-field">
-                        <label>Telpon <span class="red">*</span></label>
-                        <div class="col-xs">
-                        <input name="tc" type="text">
-                      </div>
-                      </div>
-                    </div>
-                    <div class="col-md-12">
-                      <div class="form-field">
-                        <label>Password <span class="red">*</span></label>
-                        <div class="col-xs">
-                        <input name="sc" type="text">
-                      </div>
-                      </div>
-                    </div>
-                    <div class="col-lg-12">
-                      <div class="form-field">
-                        <label>Konfirmasi Password <span class="red">*</span></label>
-                        <div class="col-xs">
-                        <input name="pz" type="text">
+                      <div class="row">
+                        <div class="field col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                          <label for="ttl">Email</label>
+                          <input class="field_custom" placeholder="Email" type="text" required>
+                        </div>
+                        <div class="field col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                          <label for="ttl">Telepon</label>
+                          <input class="field_custom" placeholder="Telepon" type="text" required>
                         </div>
                       </div>
-                    </div>
-                    <div class="center">
-                      <button class="btn main_bt">Simpan</button>
-                    </div>
-                    </div>
-                    <div class="center">
-                      <div class="checkbox">
-                        <label>
-                          <h3 class="small text-center">Silahkan lengkapi data diri anda <a href="#">disini</a></h3>
-                        </label>
-                      </div>      
-                    </div>             
-                  </fieldset>
-                </form>
+                      <div class="row">
+                        <div class="field col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                          <label for="ttl">Kata Sandi</label>
+                          <input class="field_custom" placeholder="Kata Sandi" type="password" required>
+                        </div>
+                        <div class="field col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                          <label for="ttl">Konfirmasi Kata Sandi</label>
+                          <input class="field_custom" placeholder="Konfirmasi Kata Sandi" type="password" required>
+                        </div>
+                      </div>
+                      <!-- <div class="col">
+                          <div class="form-group">
+                            <div class="checkbox">
+                              <label>
+                                <h3 class="small text-center">Silahkan masukan Username dan Password yang valid</h3>
+                              </label>
+                            </div>
+                          </div>
+                        </div> -->
+                      <div class="center">
+                        <button class="btn main_bt">Daftar</button>
+                      </div>
+                  </div>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
@@ -127,21 +113,9 @@
       </div>
     </div>
   </div>
-</div>            
-<!-- section -->
-
-<!-- new -->
-<div class="col-sm-12 " >
-    <div class="payment-form">
-        <div class="col-xs-6 col-md-6">
-            <!-- CREDIT CARD FORM STARTS HERE -->
-            
-            
-            
-        </div>
-    </div>
 </div>
-
+</div>
+<!-- section -->
 
 
 
@@ -174,4 +148,5 @@
 <!-- map js -->
 
 </body>
+
 </html>
