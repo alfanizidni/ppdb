ca734de1c6fe8858e0ce0c91cee93beedd14035a
ainunkarima <aynunkarima@gmail.com>
2020-12-21 12:38:36 +0700

    footer

--- application/views/admin/_partials/footer.php
+++ application/views/admin/_partials/footer.php
@@ -1,18 +1,18 @@
 <footer class="footer_style_2">
-  <div class="col-md-auto">
-     <div class="center">
-       <div class="bg-footer-top">
-          <div class="container">
-            <div class="row">
-               <div class="footer-top">
-                 <div class="row">
-                    <div class="col-md-3">
+  <div class="container-fuild">
+    <div class="row">
+      <div class="map_section">
+        <div id="map"></div>
+      </div>
+      <div class="footer_blog">
+        <div class="row">
+        <div class="col-md-4">
                       <div class="footer-widgets">
                          <div class="widgets-title">
-                             <h2>Headquarter</h2>
+                             <h2>Sekolah</h2>
                          </div>
                          <address>
-                             <p>1800 dapibus a tortor pretium,<br> Integer nisl dui, ABC 12000</p>
+                             <p>Nama Lengkap Sekolah,<br> Integer nisl dui, ABC 12000</p>
                          </address>                              
                          <div class="copyright-text"> 
                               <p>Copyright &copy; 2019 Company Name</p>
@@ -25,37 +25,21 @@
                          </div>
                       </div>
                     </div>
-                    <div class="col-md-3">
+                    <div class="col-md-4">
                          <div class="footer-info">
                               <div class="section-title">
-                                   <h2>Contact Info</h2>
+                                   <h2>Alamat Sekolah</h2>
                               </div>
                               <address>
-                                   <p>+65 2244 1100, +66 1800 1100</p>
-                                   <p><a href="mailto:youremail.co">hello@youremail.co</a></p>
+                                   <p>Alamat Lengkap Sekolah</p>
+                                   <p><a href="mailto:youremail.co">Telp./Fax.:@@@</a></p>
                               </address>
                          </div>
                     </div>
-                    <div class="col-md-3">
-                         <div class="footer-info newsletter-form">
-                              <div class="section-title">
-                                   <h2>Newsletter Signup</h2>
-                              </div>
-                              <div>
-                                   <div class="form-group">
-                                        <!-- <form action="#" method="get">
-                                             <input type="email" class="form-control" placeholder="Enter your email" name="email" id="email" required="">
-                                             <input type="submit" class="form-control" name="submit" id="form-submit" value="Send me">
-                                        </form> -->
-                                        <span><sup>*</sup> Please note - we do not spam your email.</span>
-                                   </div>
-                              </div>
-                         </div>
-                    </div>
-                    <div class="col-md-3">
+                    <div class="col-md-4">
                          <div class="footer-info">
                               <div class="section-title">
-                                   <h2>Address</h2>
+                                   <h2>Info Kontak Utama</h2>
                               </div>
                               <address>
                                    <p>+65 2244 1100, +66 1800 1100</p>
@@ -63,12 +47,119 @@
                               </address>
                          </div>
                      </div>
-                  </div> 
-               </div>
-            </div>      
-          </div>
-       </div>
-     </div>
+                  </div>
+        </div>
+      </div>
+    </div>
   </div>
-</div>         
-</footer> 
+</footer>
+<!-- end footer -->
+<!-- map js -->
+<script>
+         // This example adds a marker to indicate the position of Bondi Beach in Sydney,
+         // Australia.
+         function initMap() {
+           var map = new google.maps.Map(document.getElementById('map'), {
+             zoom: 11,
+             center: {lat: 40.645037, lng: -73.880224},
+         styles: [
+                  {
+                    elementType: 'geometry',
+                    stylers: [{color: '#fefefe'}]
+                  },
+                  {
+                    elementType: 'labels.icon',
+                    stylers: [{visibility: 'off'}]
+                  },
+                  {
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#616161'}]
+                  },
+                  {
+                    elementType: 'labels.text.stroke',
+                    stylers: [{color: '#f5f5f5'}]
+                  },
+                  {
+                    featureType: 'administrative.land_parcel',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#bdbdbd'}]
+                  },
+                  {
+                    featureType: 'poi',
+                    elementType: 'geometry',
+                    stylers: [{color: '#eeeeee'}]
+                  },
+                  {
+                    featureType: 'poi',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#757575'}]
+                  },
+                  {
+                    featureType: 'poi.park',
+                    elementType: 'geometry',
+                    stylers: [{color: '#e5e5e5'}]
+                  },
+                  {
+                    featureType: 'poi.park',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#9e9e9e'}]
+                  },
+                  {
+                    featureType: 'road',
+                    elementType: 'geometry',
+                    stylers: [{color: '#eee'}]
+                  },
+                  {
+                    featureType: 'road.arterial',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#3d3523'}]
+                  },
+                  {
+                    featureType: 'road.highway',
+                    elementType: 'geometry',
+                    stylers: [{color: '#eee'}]
+                  },
+                  {
+                    featureType: 'road.highway',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#616161'}]
+                  },
+                  {
+                    featureType: 'road.local',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#9e9e9e'}]
+                  },
+                  {
+                    featureType: 'transit.line',
+                    elementType: 'geometry',
+                    stylers: [{color: '#e5e5e5'}]
+                  },
+                  {
+                    featureType: 'transit.station',
+                    elementType: 'geometry',
+                    stylers: [{color: '#000'}]
+                  },
+                  {
+                    featureType: 'water',
+                    elementType: 'geometry',
+                    stylers: [{color: '#c8d7d4'}]
+                  },
+                  {
+                    featureType: 'water',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#b1a481'}]
+                  }
+                ]
+         });
+         
+           var image = 'images/it_service/location_icon_map_cont.png';
+           var beachMarker = new google.maps.Marker({
+             position: {lat: 40.645037, lng: -73.880224},
+             map: map,
+             icon: image
+           });
+         }
+      </script>
+<!-- google map js -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8eaHt9Dh5H57Zh0xVTqxVdBFCvFMqFjQ&callback=initMap"></script>
+<!-- end google map js -->
